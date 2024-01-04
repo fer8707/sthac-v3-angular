65175fa2ab4583afd47907ff3a27db20e489f86f
fer8707 <fer8707@gmail.com>
2024-01-04 00:20:26 -0600

    nav icons and carrousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavIconsComponent } from './components/home/nav-icons/nav-icons.component';
+import { CarrouselComponent } from './components/home/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavIconsComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/home/carrousel/carrousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-carrousel',
+  templateUrl: './carrousel.component.html',
+  styleUrls: ['./carrousel.component.css']
+})
+export class CarrouselComponent {
+
+}
